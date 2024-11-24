bef30a38ca001ce3c5c2212f46f722d7133b5636
gdyun_bxrc6pf <g.dyunin@gmail.com>
2024-11-24 15:16:16 +0300

    iter3

--- internal/server/handlers/metric_get.go
+++ internal/server/handlers/metric_get.go
@@ -1,12 +1,36 @@
 package handlers
 
 import (
+	"github.com/gdyunin/metricol.git/internal/server/metrics"
 	"github.com/gdyunin/metricol.git/internal/server/storage"
+	"github.com/go-chi/chi/v5"
 	"net/http"
 )
 
 func MetricGetHandler(repository storage.Repository) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Check Content-Type
+		if r.Header.Get("Content-Type") != "text/plain" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// Parse params from URL
+		metricType := chi.URLParam(r, "metricType")
+		metricName := chi.URLParam(r, "metricName")
+
+		metricValue, ok := repository.Metrics()[metrics.MetricType(metricType)][metricName]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		_, _ = w.Write([]byte(metricValue))
+
+		// Headers
+		h := w.Header()
+		h.Set("Content-Type", "text/plain")
+
 		// Response
 		w.WriteHeader(http.StatusOK)
 	}
