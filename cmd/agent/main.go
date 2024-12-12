f7c1b81b40a774787055a65745e86a37c4234eaf
gdyun_bxrc6pf <g.dyunin@gmail.com>
2024-12-12 12:28:03 +0300

    Refactoring of the metricol/cmd/agent/main.go

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -8,35 +8,49 @@ import (
 	agentcfg "github.com/gdyunin/metricol.git/internal/config/agent"
 )
 
-// main is the entry point of the application.
-// It initializes the agent configuration and starts the polling and reporting routines.
 func main() {
 	var workGroup sync.WaitGroup
-	workGroup.Add(1) // If at least one goroutine ends, stop the application.
+	workGroup.Add(1)
 
-	// Parse the application configuration.
+	// Retrieve the application configuration.
+	appCfg := appConfig()
+	// Create a default agent with the application configuration.
+	a := agent.DefaultAgent(appCfg)
+
+	// Start polling and reporting routines with the wait group.
+	runRoutinesWithWG(
+		&workGroup,
+		func() {
+			a.Polling(appCfg.PollInterval)
+		},
+		func() {
+			a.Reporting(appCfg.ReportInterval)
+		},
+	)
+
+	// Wait for all routines to finish before logging an error message.
+	workGroup.Wait()
+	log.Fatal("Polling or sending was interrupted, the agent was stopped.")
+}
+
+// appConfig retrieves and parses the application configuration.
+// It logs a fatal error if the configuration cannot be parsed.
+func appConfig() *agentcfg.Config {
 	appCfg, err := agentcfg.ParseConfig()
 	if err != nil {
 		log.Fatalf("Get configuration fail: %v", err)
 	}
+	return appCfg
+}
 
-	// Create a new instance of the agent with the parsed configuration.
-	a := agent.DefaultAgent(appCfg)
-
-	// Start the polling in a separate goroutine.
-	go func() {
-		defer workGroup.Done()
-		a.Polling(appCfg.PollInterval)
-	}()
-
-	// Start the reporting in a separate goroutine.
-	go func() {
-		defer workGroup.Done()
-		a.Reporting(appCfg.ReportInterval)
-	}()
-
-	// Wait for at least one goroutine to complete.
-	workGroup.Wait()
-	// Application stopped, log it and exit,
-	log.Fatal("Polling or sending was interrupted, the agent was stopped.")
+// runRoutinesWithWG starts multiple functions as goroutines and waits for them to complete.
+// Each function is executed in its own goroutine, and the wait group is used to ensure
+// that the main function waits for all routines to finish before proceeding.
+func runRoutinesWithWG(wg *sync.WaitGroup, fn ...func()) {
+	for _, f := range fn {
+		go func(f func()) {
+			defer wg.Done()
+			f()
+		}(f) // Pass f as an argument to avoid closure issues
+	}
 }
