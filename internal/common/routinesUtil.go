e3fa0262f95f8dc8dbb67a0b797f162dd8d0aa5f
gdyun_bxrc6pf <g.dyunin@gmail.com>
2024-12-12 12:39:19 +0300

    Refactoring of the metricol/internal/common/routinesUtil.go

--- internal/common/routinesUtil.go
+++ internal/common/routinesUtil.go
@@ -3,13 +3,19 @@ package common
 import "sync"
 
 // RunRoutinesWithWG starts multiple functions as goroutines and waits for them to complete.
+// It takes a wait group and a variadic list of functions to be executed concurrently.
 // Each function is executed in its own goroutine, and the wait group is used to ensure
 // that the main function waits for all routines to finish before proceeding.
+//
+// Parameters:
+//
+//	wg: A pointer to a sync.WaitGroup that is used to synchronize the completion of the goroutines.
+//	fn: A variadic list of functions to be executed concurrently.
 func RunRoutinesWithWG(wg *sync.WaitGroup, fn ...func()) {
 	for _, f := range fn {
-		go func(f func()) {
-			defer wg.Done()
-			f()
-		}(f) // Pass f as an argument to avoid closure issues
+		go func() {
+			defer wg.Done() // Decrement the wait group counter when the goroutine completes.
+			f()             // Execute the function.
+		}()
 	}
 }
