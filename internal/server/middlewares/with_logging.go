bb292c404ea237fc36437d24d68867f3a645b475
gdyun_bxrc6pf <g.dyunin@gmail.com>
2024-12-16 11:43:19 +0300

    Create middleware in with_logging.go for log requests; Update go.mod and go.sum.

---
+++ internal/server/middlewares/with_logging.go
@@ -0,0 +1,57 @@
+package middlewares
+
+import (
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+// responseInfo holds information about the HTTP response status and size.
+type responseInfo struct {
+	status int // HTTP status code of the response.
+	size   int // Size of the response in bytes.
+}
+
+// loggingResponseWriter wraps a http.ResponseWriter to capture response information.
+type loggingResponseWriter struct {
+	http.ResponseWriter               // The original ResponseWriter.
+	responseInfo        *responseInfo // Pointer to the responseInfo struct.
+}
+
+// Write writes the data to the ResponseWriter and updates the response size.
+func (rw *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := rw.ResponseWriter.Write(b)
+	rw.responseInfo.size += size // Update the size of the response.
+	return size, err
+}
+
+// WriteHeader sends an HTTP response header and updates the response status.
+func (rw *loggingResponseWriter) WriteHeader(statusCode int) {
+	rw.ResponseWriter.WriteHeader(statusCode)
+	rw.responseInfo.status = statusCode // Update the status code of the response.
+}
+
+// WithLogging is a middleware that logs HTTP requests and responses.
+func WithLogging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()          // Record the start time of the request.
+		var logger zap.SugaredLogger // Logger for recording log messages. TODO: Initialize logger properly.
+
+		lw := loggingResponseWriter{
+			ResponseWriter: w,
+			responseInfo:   &responseInfo{},
+		}
+
+		next.ServeHTTP(&lw, r) // Call the next handler in the chain.
+
+		logger.Infoln(
+			"Received HTTP request:",
+			"method", r.Method,
+			"uri", r.RequestURI,
+			"processed time", time.Since(start), // Calculate the processing time.
+			"response code", lw.responseInfo.status,
+			"response size", lw.responseInfo.size,
+		)
+	})
+}
