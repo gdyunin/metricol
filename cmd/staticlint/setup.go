7c960d5a83d0b507fef0b072ce02242ab6bb5672
German Dyunin <g.dyunin@gmail.com>
2025-04-08 17:45:37 +0300

    refact staticlint

---
+++ cmd/staticlint/setup.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"strings"
+
+	gocritic "github.com/go-critic/go-critic/checkers/analyzer"
+	"github.com/kisielk/errcheck/errcheck"
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/asmdecl"
+	"golang.org/x/tools/go/analysis/passes/assign"
+	"golang.org/x/tools/go/analysis/passes/atomic"
+	"golang.org/x/tools/go/analysis/passes/bools"
+	"golang.org/x/tools/go/analysis/passes/buildtag"
+	"golang.org/x/tools/go/analysis/passes/cgocall"
+	"golang.org/x/tools/go/analysis/passes/composite"
+	"golang.org/x/tools/go/analysis/passes/copylock"
+	"golang.org/x/tools/go/analysis/passes/deepequalerrors"
+	"golang.org/x/tools/go/analysis/passes/errorsas"
+	"golang.org/x/tools/go/analysis/passes/fieldalignment"
+	"golang.org/x/tools/go/analysis/passes/httpresponse"
+	"golang.org/x/tools/go/analysis/passes/loopclosure"
+	"golang.org/x/tools/go/analysis/passes/lostcancel"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
+	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/shadow"
+	"golang.org/x/tools/go/analysis/passes/shift"
+	"golang.org/x/tools/go/analysis/passes/sortslice"
+	"golang.org/x/tools/go/analysis/passes/stdmethods"
+	"golang.org/x/tools/go/analysis/passes/stringintconv"
+	"golang.org/x/tools/go/analysis/passes/structtag"
+	"golang.org/x/tools/go/analysis/passes/tests"
+	"golang.org/x/tools/go/analysis/passes/unmarshal"
+	"golang.org/x/tools/go/analysis/passes/unreachable"
+	"golang.org/x/tools/go/analysis/passes/unsafeptr"
+	"golang.org/x/tools/go/analysis/passes/unusedresult"
+	"honnef.co/go/tools/staticcheck"
+)
+
+func makeAlalyzersSlice() []*analysis.Analyzer {
+	checks := make([]*analysis.Analyzer, 0)
+
+	checks = append(checks, staticChecks()...)
+	checks = append(checks, otherChecks()...)
+	checks = append(checks, analysisChecks()...)
+
+	return checks
+}
+
+func staticChecks() []*analysis.Analyzer {
+	checks := make([]*analysis.Analyzer, 0)
+	includedStaticChecks := []string{
+		"SA", "S1012", "ST1005", "QF1011",
+	}
+
+	for _, v := range staticcheck.Analyzers {
+		for _, checkName := range includedStaticChecks {
+			if strings.HasPrefix(v.Analyzer.Name, checkName) {
+				checks = append(checks, v.Analyzer)
+				break
+			}
+		}
+	}
+
+	return checks
+}
+
+func otherChecks() []*analysis.Analyzer {
+	return []*analysis.Analyzer{
+		errcheck.Analyzer,
+		gocritic.Analyzer,
+	}
+}
+
+func analysisChecks() []*analysis.Analyzer {
+	return []*analysis.Analyzer{
+		asmdecl.Analyzer,
+		assign.Analyzer,
+		atomic.Analyzer,
+		bools.Analyzer,
+		buildtag.Analyzer,
+		cgocall.Analyzer,
+		composite.Analyzer,
+		copylock.Analyzer,
+		deepequalerrors.Analyzer,
+		errorsas.Analyzer,
+		fieldalignment.Analyzer,
+		httpresponse.Analyzer,
+		loopclosure.Analyzer,
+		lostcancel.Analyzer,
+		nilfunc.Analyzer,
+		printf.Analyzer,
+		shadow.Analyzer,
+		shift.Analyzer,
+		sortslice.Analyzer,
+		stdmethods.Analyzer,
+		stringintconv.Analyzer,
+		structtag.Analyzer,
+		tests.Analyzer,
+		unmarshal.Analyzer,
+		unreachable.Analyzer,
+		unsafeptr.Analyzer,
+		unusedresult.Analyzer,
+	}
+}
