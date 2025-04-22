cc08f874dc957e3e17e213e1b0d7c96b2abcac00
German Dyunin <g.dyunin@gmail.com>
2025-04-22 22:03:06 +0300

    Revert "add read json to config"
    This reverts commit 42aa9000081fb2d8095dee0f9acdecc0da1e8106.

--- internal/agent/config/config.go
+++ internal/agent/config/config.go
@@ -5,10 +5,8 @@
 package config
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"os"
 
 	"github.com/caarlos0/env/v6"
 )
@@ -28,13 +26,13 @@ const (
 // It contains the server address, signing key, intervals for polling and reporting metrics,
 // a rate limit for HTTP requests, and a flag for enabling or disabling pprof profiling.
 type Config struct {
-	ServerAddress  string `env:"ADDRESS"         json:"server_address"`
-	SigningKey     string `env:"KEY"             json:"signing_key"`
-	CryptoKey      string `env:"CRYPTO_KEY"      json:"crypto_key"`
-	PollInterval   int    `env:"POLL_INTERVAL"   json:"poll_interval"`
-	ReportInterval int    `env:"REPORT_INTERVAL" json:"report_interval"`
-	RateLimit      int    `env:"RATE_LIMIT"      json:"rate_limit"`
-	PprofFlag      bool   `env:"PPROF_FLAG"      json:"pprof_flag"`
+	ServerAddress  string `env:"ADDRESS"`
+	SigningKey     string `env:"KEY"`
+	CryptoKey      string `env:"CRYPTO_KEY"`
+	PollInterval   int    `env:"POLL_INTERVAL"`
+	ReportInterval int    `env:"REPORT_INTERVAL"`
+	RateLimit      int    `env:"RATE_LIMIT"`
+	PprofFlag      bool   `env:"PPROF_FLAG"`
 }
 
 // ParseConfig initializes a new Config instance with default values, then overrides these values
@@ -45,20 +43,6 @@ type Config struct {
 // Returns:
 //   - *Config: A pointer to the populated Config structure.
 //   - error: An error if environment variable parsing fails; otherwise, nil.
-//
-// ParseConfig initializes and returns a Config object by parsing configuration
-// from multiple sources in the following order of precedence:
-// 1. Default settings are applied.
-// 2. Configuration file values are parsed and override defaults.
-// 3. Command-line arguments are parsed and override previous values.
-// 4. Environment variables are parsed and override previous values.
-//
-// If any error occurs during the parsing of the configuration file or
-// environment variables, it returns an error.
-//
-// Returns:
-// - A pointer to the populated Config object.
-// - An error if parsing fails at any stage.
 func ParseConfig() (*Config, error) {
 	// Default settings for the service configuration.
 	cfg := Config{
@@ -71,10 +55,6 @@ func ParseConfig() (*Config, error) {
 		CryptoKey:      defaultCryptoKey,
 	}
 
-	if err := parseConfigFile(&cfg); err != nil {
-		return nil, fmt.Errorf("error parsing config file: %w", err)
-	}
-
 	// Parse command-line arguments or set default settings if no arguments are provided.
 	parseFlagsOrSetDefault(&cfg)
 
@@ -84,45 +64,6 @@ func ParseConfig() (*Config, error) {
 	}
 
 	return &cfg, nil
-}
-
-// parseConfigFile parses the configuration for the application.
-// It first attempts to read the configuration file path from the command-line
-// flag "-c". If the flag is not provided, it falls back to the "CONFIG"
-// environment variable. If a configuration file path is found, it reads the
-// file and unmarshals its JSON content into the provided Config struct.
-//
-// Parameters:
-//   - cfg: A pointer to a Config struct where the parsed configuration will be stored.
-//
-// Returns:
-//   - An error if there is an issue with parsing command-line flags, reading the
-//     configuration file, or unmarshaling the JSON content. Returns nil if the
-//     configuration is successfully parsed.
-func parseConfigFile(cfg *Config) error {
-	var configPath string
-
-	fs := flag.NewFlagSet("config", flag.ContinueOnError)
-	fs.StringVar(&configPath, "c", "", "Path to JSON config file")
-	if err := fs.Parse(os.Args[1:]); err != nil {
-		return fmt.Errorf("error parsing command-line flags: %w", err)
-	}
-
-	if configPath == "" {
-		configPath = os.Getenv("CONFIG")
-	}
-
-	if configPath != "" {
-		data, err := os.ReadFile(configPath)
-		if err != nil {
-			return fmt.Errorf("failed to read config file: %w", err)
-		}
-
-		if err := json.Unmarshal(data, cfg); err != nil {
-			return fmt.Errorf("cannot parse JSON config %q: %w", configPath, err)
-		}
-	}
-	return nil
 }
 
 // parseFlagsOrSetDefault populates the Config structure with values provided as command-line flags.
--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -3,10 +3,8 @@
 package config
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"os"
 
 	"github.com/caarlos0/env/v6"
 )
@@ -28,14 +26,14 @@ const (
 // The configuration values can be provided via environment variables, command-line flags,
 // or default settings defined in the package.
 type Config struct {
-	ServerAddress   string `env:"ADDRESS"           json:"server_address"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH" json:"file_storage_path"`
-	DatabaseDSN     string `env:"DATABASE_DSN"      json:"database_dsn"`
-	SigningKey      string `env:"KEY"               json:"signing_key"`
-	CryptoKey       string `env:"CRYPTO_KEY"        json:"crypto_key"`
-	StoreInterval   int    `env:"STORE_INTERVAL"    json:"store_interval"`
-	Restore         bool   `env:"RESTORE"           json:"restore"`
-	PprofFlag       bool   `env:"PPROF_SERVER_FLAG" json:"pprof_flag"`
+	ServerAddress   string `env:"ADDRESS"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH"`
+	DatabaseDSN     string `env:"DATABASE_DSN"`
+	SigningKey      string `env:"KEY"`
+	CryptoKey       string `env:"CRYPTO_KEY"`
+	StoreInterval   int    `env:"STORE_INTERVAL"`
+	Restore         bool   `env:"RESTORE"`
+	PprofFlag       bool   `env:"PPROF_SERVER_FLAG"`
 }
 
 // ParseConfig initializes the Config with default values, overrides them with command-line flags if provided,
@@ -45,35 +43,6 @@ type Config struct {
 // Returns:
 //   - *Config: A pointer to the populated Config structure.
 //   - error: An error if parsing of environment variables fails.
-//
-// ParseConfig initializes and returns a Config struct by parsing configuration
-// values from multiple sources in the following order of precedence:
-// 1. Default settings defined in the code.
-// 2. Configuration file, if available.
-// 3. Command-line flags, which override the previous values.
-// 4. Environment variables, which override all previous values.
-//
-// If any error occurs during parsing the configuration file or environment
-// variables, it returns an error.
-//
-// Returns:
-//   - A pointer to the populated Config struct.
-//   - An error if parsing fails at any stage.
-//
-// ParseConfig initializes and returns a Config object by combining default values,
-// configuration file settings, command-line flags, and environment variables.
-//
-// The function performs the following steps:
-//  1. Initializes a Config object with default values.
-//  2. Attempts to parse a configuration file to override default values.
-//     If parsing the file fails, an error is returned.
-//  3. Parses command-line flags to override existing configuration values or set defaults.
-//  4. Parses environment variables to override configuration values if they are set.
-//     If parsing environment variables fails, an error is returned.
-//
-// Returns:
-// - A pointer to the populated Config object.
-// - An error if any step in the configuration process fails.
 func ParseConfig() (*Config, error) {
 	// Default settings for the server configuration.
 	cfg := Config{
@@ -87,10 +56,6 @@ func ParseConfig() (*Config, error) {
 		CryptoKey:       defaultCryptoKey,
 	}
 
-	if err := parseConfigFile(&cfg); err != nil {
-		return nil, fmt.Errorf("error parsing config file: %w", err)
-	}
-
 	// Populate the configuration from command-line flags.
 	parseFlagsOrSetDefault(&cfg)
 
@@ -100,45 +65,6 @@ func ParseConfig() (*Config, error) {
 	}
 
 	return &cfg, nil
-}
-
-// parseConfigFile parses the configuration for the application.
-// It first attempts to read the configuration file path from the command-line
-// flag "-c". If the flag is not provided, it falls back to the "CONFIG"
-// environment variable. If a configuration file path is found, it reads the
-// file and unmarshals its JSON content into the provided Config struct.
-//
-// Parameters:
-//   - cfg: A pointer to a Config struct where the parsed configuration will be stored.
-//
-// Returns:
-//   - An error if there is an issue with parsing command-line flags, reading the
-//     configuration file, or unmarshaling the JSON content. Returns nil if the
-//     configuration is successfully parsed.
-func parseConfigFile(cfg *Config) error {
-	var configPath string
-
-	fs := flag.NewFlagSet("config", flag.ContinueOnError)
-	fs.StringVar(&configPath, "c", "", "Path to JSON config file")
-	if err := fs.Parse(os.Args[1:]); err != nil {
-		return fmt.Errorf("error parsing command-line flags: %w", err)
-	}
-
-	if configPath == "" {
-		configPath = os.Getenv("CONFIG")
-	}
-
-	if configPath != "" {
-		data, err := os.ReadFile(configPath)
-		if err != nil {
-			return fmt.Errorf("failed to read config file: %w", err)
-		}
-
-		if err := json.Unmarshal(data, cfg); err != nil {
-			return fmt.Errorf("cannot parse JSON config %q: %w", configPath, err)
-		}
-	}
-	return nil
 }
 
 // parseFlagsOrSetDefault populates the Config from command-line flags,
